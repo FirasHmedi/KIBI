c8278400965c806a45802e6e244a3b233d598535
Firas Hmedi <hmedi.firas.hf@gmail.com>
2023-10-05 00:00:44 +0200

    set 6hp

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -16,7 +16,7 @@ export const GAMES_PATH = 'games/';
 
 export const ROUND_DURATION = 80; //60
 
-export const INITIAL_HP = 8;
+export const INITIAL_HP = 6;
 
 export const ENV_MAX_LOAD = 3; // each turn get 1
 
